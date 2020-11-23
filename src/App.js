7aae9241c026abab7fff33f3901e54d9a7a81e4a
SeungminKim <ksm0057@naver.com>
2020-11-23 22:51:18 +0900

    prototype1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import html2canvas from "html2canvas";
 
 function App() {
+  const captureClicked = (event) => {
+    html2canvas(document.querySelector("#myCanvas"), {}).then((canvas) => {
+      saveAs(canvas.toDataURL(), "sdf.png");
+    });
+  };
+  const saveAs = (uri, filename) => {
+    const link = document.createElement("a");
+    if (typeof link.download === "string") {
+      link.href = uri;
+      link.download = filename;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      window.open(uri);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div
+        className="container"
+        id="myCanvas"
+        style={{ border: "1px solid pink", width: "350px", height: "300px" }}
+      >
+        <div
+          className="square"
+          style={{ border: "1px solid aqua", width: "300px", height: "350px" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          square
+        </div>
+      </div>
+      <button onClick={captureClicked}>Capture</button>
+    </>
   );
 }
 
